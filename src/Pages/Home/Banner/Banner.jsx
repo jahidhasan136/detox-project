4edecc3bab872a17596c788c4b5165c40cfc9caf
Jahid Hasan <jh3495689@gmail.com>
2023-06-23 00:39:18 +0600

    banner section complete

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+
+
+const Banner = () => {
+    return (
+        <div className="text-center w-1/3 mx-auto mt-28">
+            <span className="text-lg font-medium px-5 py-2 border rounded-full capitalize">🔥 Powered by Graphchain tech</span>
+            <h1 className="text-6xl font-bold mt-7">Lybell gc is fast, scalable, robust, green and cross-chain</h1>
+            <p className="mt-9">By allowing a DAG of data istead of a rigid Blockchain we include all users in every transaction...? something about whatactually happens here</p>
+            <button className="btn_custom mt-10">Get Started</button>
+        </div>
+    );
+};
+
+export default Banner;
