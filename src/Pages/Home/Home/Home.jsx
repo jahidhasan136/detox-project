40ee78d64d1c7262748e2d00dd458b96a5477b86
Jahid Hasan <jh3495689@gmail.com>
2023-06-23 01:09:17 +0600

    card section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import BlockchainPlatform from "../BlockchainPlatform/BlockchainPlatform";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <BlockchainPlatform></BlockchainPlatform>
         </div>
     );
 };
