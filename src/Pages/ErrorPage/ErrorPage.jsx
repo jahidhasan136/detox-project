63e089a414b7a8780a363d125408c26621d5ca26
Jahid Hasan <jh3495689@gmail.com>
2023-06-24 01:14:33 +0600

    404 route complete

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
-        <div>
-            
+        <div className="grid justify-center mt-80 text-center">
+            <h1 className="text-9xl font-bold mb-8 text-red-600">404 Error</h1>
+            <h2 className="text-2xl font-semibold mb-3">Oops! The Lybell is not around</h2>
+            <p className="text-lg mb-2">We tried but we couldn&#39;t find the page you&#39;re looking for.</p>
+            <p className="text-lg mb-6">We want to help you the best we can. You&#39;re invited to our homepage.</p>
+            <Link to='/'><button className="btn_custom">Back to Home</button></Link>
         </div>
     );
 };
--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -3,10 +3,12 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
   export const router = createBrowserRouter([
     {
       path: "/",
+      errorElement: <ErrorPage></ErrorPage>,
       element: <Main></Main>,
       children: [
         {
